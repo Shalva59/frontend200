5542568112b5a65df83021221b203c0dda269d4d
shakosmail59 <shakosmail12345@gmail.com>
2024-02-20 02:11:19 +0400

    scsac

--- N12_Movie_Test/src/js/buyTicket.js
+++ N12_Movie_Test/src/js/buyTicket.js
@@ -1,24 +1,32 @@
 const formID = document.getElementById("form");
-
 let sum = 0;
-const payMoney = 10;
-for (let i = 1; i <= 10; i++) {
-const createDiv = document.createElement("div");
+const payMoney = 
+[
+     {id:1,price:10},
+     {id:2,price:20},
+     {id:3,price:30},
+     {id:4,price:40},
+     {id:5,price:50},
+     {id:6,price:60},
+     {id:7,price:70},
+     {id:8,price:80},
+     {id:9,price:90},
+     {id:10,price:100}
+]
 
-    createDiv.innerHTML = `
-    <div class="form-check">
-    <input class="form-check-input" type="checkbox" value="${i*10}" id="flexCheckDefault">
-    <label class="form-check-label" for="flexCheckDefault">A${i}</label>
-    </div>
-    `
-    formID.appendChild(createDiv);
-}
 
-const btnShow = document.querySelector("btnShow");
-const result = document.querySelector("top");
 
-btnShow.addEventListener("click",()=>
+payMoney.map((element)=>
 {
-    let checkbox = document.querySelector('input[type=checkbox]:checked');
-    result.innerHTML= checkbox.value;
-});
+    const form = document.getElementById("form");
+    const checkboxInnerHTML = `<input type="checkbox" id="accept" name="accept" value="${element.id}">
+    <label>${element.price}</label>` 
+
+    const new_div=document.createElement("div");
+    new_div.classList.add("checkboxDIV");
+
+    new_div.innerHTML = checkboxInnerHTML;
+
+    form.appendChild(new_div);
+
+})
---
+++ N13_Pokemon_Api/description.js
@@ -0,0 +1,79 @@
+const id = new URLSearchParams(window.location.search).get("id");
+
+const poke_container = document.getElementById("poke_container");
+
+const colours = {
+    normal: '#A8A77A',
+    fire: '#EE8130',
+    water: '#6390F0',
+    electric: '#F7D02C',
+    grass: '#7AC74C',
+    ice: '#96D9D6',
+    fighting: '#C22E28',
+    poison: '#A33EA1',
+    ground: '#E2BF65',
+    flying: '#A98FF3',
+    psychic: '#F95587',
+    bug: '#A6B91A',
+    rock: '#B6A136',
+    ghost: '#735797',
+    dragon: '#6F35FC',
+    dark: '#705746',
+    steel: '#B7B7CE',
+    fairy: '#D685AD',
+};
+
+const main_types = Object.keys(colours);
+
+
+const fetchPokemons = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    createPokemonCard(data);
+}
+
+fetchPokemons(id);
+
+
+const createPokemonCard = (pokemon) => {
+    // console.log(pokemon)
+
+    const pokemonEl = document.createElement("div");
+    pokemonEl.classList.add("pokemon");
+
+    const poke_types = pokemon.types.map((type) => type.type.name);
+
+    const type = main_types.find((type) => poke_types.indexOf(type) > -1);
+    console.log(type);
+
+    //console.log(poke_types);
+
+    const color = colours[type];
+    pokemonEl.style.backgroundColor = color;
+
+    const id = pokemon.id.toString().padStart(3, "0");
+
+    const pokemonInnerHtml =
+        `
+    <div class="img-container">
+        <img src="${pokemon.sprites.front_default}" alt="">
+    </div>
+
+    <div class="info">
+        <span class="number">#${id}</span>
+        <h3 class="name">${pokemon.name}</h3>
+    </div>
+    `;
+
+
+    pokemonEl.addEventListener("click", () => {
+        window.location.href = `description.html?id=${pokemon.id}`;
+    });
+
+
+    pokemonEl.innerHTML = pokemonInnerHtml;
+
+    poke_container.appendChild(pokemonEl);
+}
