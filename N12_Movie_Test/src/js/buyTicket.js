59959d03bb1f241db471bf5074627b19416bc92c
shakosmail59 <shakosmail12345@gmail.com>
2024-02-23 23:42:06 +0400

    dasd
    asdasd

--- N12_Movie_Test/src/js/buyTicket.js
+++ N12_Movie_Test/src/js/buyTicket.js
@@ -1,32 +1,110 @@
 const formID = document.getElementById("form");
 let sum = 0;
-const payMoney = 
-[
-     {id:1,price:10},
-     {id:2,price:20},
-     {id:3,price:30},
-     {id:4,price:40},
-     {id:5,price:50},
-     {id:6,price:60},
-     {id:7,price:70},
-     {id:8,price:80},
-     {id:9,price:90},
-     {id:10,price:100}
+
+const payMoney = [{
+        id: 1,
+        price: 10
+    },
+    {
+        id: 2,
+        price: 20
+    },
+    {
+        id: 3,
+        price: 30
+    },
+    {
+        id: 4,
+        price: 40
+    },
+    {
+        id: 5,
+        price: 50
+    },
+    {
+        id: 6,
+        price: 60
+    },
+    {
+        id: 7,
+        price: 70
+    },
+    {
+        id: 8,
+        price: 80
+    },
+    {
+        id: 9,
+        price: 90
+    },
+    {
+        id: 10,
+        price: 100
+    },
+    {
+        id: 11,
+        price: 110
+    },
+    {
+        id: 12,
+        price: 120
+    },
+
+    {
+        id: 13,
+        price: 130
+    },
+    {
+        id: 14,
+        price: 140
+    },
+
+    {
+        id: 15,
+        price: 150
+    },
+    {
+        id: 16,
+        price: 160
+    }
+
 ]
 
 
 
-payMoney.map((element)=>
-{
+payMoney.map((element) => {
     const form = document.getElementById("form");
-    const checkboxInnerHTML = `<input type="checkbox" id="accept" name="accept" value="${element.id}">
-    <label>${element.price}</label>` 
+    const checkboxInnerHTML = `<input type="checkbox" id="accept" name="accept" value="${element.price}">
+    <label>${element.id}</label>`
 
-    const new_div=document.createElement("div");
+    const new_div = document.createElement("div");
     new_div.classList.add("checkboxDIV");
+
 
     new_div.innerHTML = checkboxInnerHTML;
 
     form.appendChild(new_div);
+
+    let topclass = document.getElementById("top");
+
+    new_div.addEventListener("click", (e) => {
+        if (e.target.checked) {
+            sum += element.price;
+        } else {
+            sum -= element.price;
+        }
+        console.log(sum);
+
+        if(sum < 0)
+        {
+            sum = 0 ;
+
+        }
+        else
+        {
+            topclass.innerHTML = `${sum}`;
+        }
+
+    })
 
 })
